479c307340ecae53c8d6c028235012c71fa4f5bb
Abhishek MS <abhishekmschotta@gmail.com>
2024-07-24 14:17:03 +0530

    missing files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import { Typography } from '@mui/material';
-import logo from './logo.svg';
+// import { Typography } from '@mui/material';
+// import logo from './logo.svg';
 import Paymentpage from './Paymentpage/Paymentpage';
 import Preview from './Preview/Preview';
-import Planview from './Planview/Planview';
-import AboutMEEdit from './aboutmeedit/AboutMeEdit';
-import AddAchivement from './AddAchivement/AddAchivement';
-import AddOverview from './Pages/AddOverview/AddOverview';
+// import Planview from './Planview/Planview';
+// import AboutMEEdit from './aboutmeedit/AboutMeEdit';
+// import AddAchivement from './AddAchivement/AddAchivement';
+// import AddOverview from './Pages/AddOverview/AddOverview';
 import Admin from './Pages/Admin/Admin.jsx';
 import Footer from './Pages/Footer';
 import Home from './Pages/Home/Home';
-import Login from './Pages/LoginTemplates/Login';
-import LoginFind from './Pages/LoginTemplates/LoginFind';
-import Navbar from './Pages/Navbar';
-import Payment from './Pages/PaymemtSuccess/PaymentSuccess';
-import Singup from './Pages/Singup/Singup';
+// import Login from './Pages/LoginTemplates/Login';
+// import LoginFind from './Pages/LoginTemplates/LoginFind';
+// import Navbar from './Pages/Navbar';
+// import Payment from './Pages/PaymemtSuccess/PaymentSuccess';
+// import Singup from './Pages/Singup/Singup';
 import "./common.css"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Newcomp from './Newcomp.jsx';
-
 function App() {
   return (<BrowserRouter>
     {/* // <div className="App"> */}
@@ -36,17 +34,13 @@ function App() {
       <AboutMEEdit></AboutMEEdit>
       <AddAchivement/> */}
 {/* //  </div> */}
-
-
 <Routes>
-{/* <Route path="/" element={<Newcomp></Newcomp>}></Route> */}
 
   <Route path="/" element={<Home></Home>}></Route>
 
     <Route path="/admin/" element={<Admin />} />
     <Route path="/preview" element={<Preview></Preview>} />
     <Route path="/payment" element={<Paymentpage></Paymentpage>} />
-    {/* <Route path="*" element={<NoPage />} /> */}
   
 </Routes>
 <Footer></Footer> 
---
+++ src/SMSconfig/SmsConfig.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SmsConfig = () => {
+  return (
+    <div>
+      Abhishek ms
+    </div>
+  )
+}
+
+export default SmsConfig
